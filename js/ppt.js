2b7df6d151cb420d44480e50f31f8f83152e3027
elkabal82 <etvb66@hotmail.com>
2014-08-12 15:28:13 -0500

    ya terminado pero la maquina simpre escoje papel, no esta aleatorio

---
+++ js/ppt.js
@@ -0,0 +1,69 @@
+//Codigo para el juego de piedra papel o tijereas
+//declaramos las variables 
+var piedra = 0;
+var papel = 1;
+var tijera = 2;
+var opcionUsuario;
+var opcionMaquina = piedra;
+//depues del prompt no se deja ESPACIO ente el prompt y el parentesis
+//y despues del \n no dejar espacio
+//Guardamos en opcionUsuario, lo que el usuario elijio
+opcionUsuario = prompt("¿Que eliges?\nPiedra: 0\nPapel: 1\nTijera: 2", 0);
+//si el usuario elije piedra, entra aquí
+if (opcionUsuario == piedra)
+{
+	alert("Elegiste Piedra");
+
+	if (opcionMaquina == piedra)
+	{
+		alert("Empate");
+	}
+	else if (opcionMaquina == papel)
+	{
+		alert("Perdiste");
+	}
+	else if (opcionMaquina == tijera)
+	{
+		alert("Ganaste");
+	}
+}
+//si el usuario elije papel entra aquí
+else if (opcionUsuario == papel)
+{
+	alert("Elegiste papel");
+
+	if (opcionMaquina == piedra)
+	{
+		alert("Ganaste");
+	}
+	else if (opcionMaquina == papel)
+	{
+		alert("Empate");
+	}
+	else if (opcionMaquina == tijera)
+	{
+		alert("Perdiste");
+	}
+}
+//si el usuario elije tijeras entra aquí
+else if (opcionUsuario == tijera)
+{
+	alert("Elegiste tijera");
+	if (opcionMaquina == piedra)
+	{
+		alert("Perdiste");
+	}
+	else if (opcionMaquina == papel)
+	{
+		alert("Ganaste");
+	}
+	else if (opcionMaquina == tijera)
+	{
+		alert("Empate");
+	}
+}
+//si escoje una opcion que no es valida como un numero mayor de 3 entra aquí
+else
+{
+	alert("Esa opcion no existe");
+}
